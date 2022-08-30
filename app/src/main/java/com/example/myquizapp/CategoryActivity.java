c6ae5a39c1308ea7ebe8f7b87732f442d556f292
ShchegortsovaPolina <shegortsovapolina9999@gmail,com>
2022-08-30 17:23:08 +0900

    MyQuizApp

---
+++ app/src/main/java/com/example/myquizapp/CategoryActivity.java
@@ -0,0 +1,45 @@
+package com.example.myquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import static com.example.myquizapp.SplashActivity.catList;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    private GridView catGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Categories");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        catGrid = findViewById(R.id.catGridview);
+
+
+        CatGridAdapter adapter = new CatGridAdapter(catList);
+        catGrid.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home)
+        {
+            CategoryActivity.this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
